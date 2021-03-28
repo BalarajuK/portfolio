fc4270a8d87dadf3dc8cc22aff1be67bf7805fac
bkadukun <balaraju_kadukuntla@mentor.com>
2021-03-29 01:23:28 +0530

    Added header and title

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import Config from '../constants/config.json'
+import Config from '../constants/config.json';
+import Header from './Header.js';
 
 class App extends Component {
+    componentDidMount() {
+        document.title = Config.Name + "'s Portfolio"
+    }
+
     render() {
         return <div className="App">
-            Hello World
-            <pre className="border rounded bg-light p-2">{JSON.stringify(Config, null, 2)}
-            </pre>
+            <Header>{Config.Name}</Header>
         </div>;
     }
 }
