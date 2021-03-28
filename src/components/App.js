4a5e782ec84ea9dfdfca24ab5af766b63218f137
bkadukun <balaraju_kadukuntla@mentor.com>
2021-03-29 01:52:15 +0530

    Changed header theme

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import Config from '../constants/config.json';
 import Header from './Header.js';
 
 class App extends Component {
+    state = {
+        Dark : false
+    }
     componentDidMount() {
         document.title = Config.Name + "'s Portfolio"
     }
 
     render() {
         return <div className="App">
-            <Header>{Config.Name}</Header>
+            <Header className="Header" dark={this.state.Dark}>{Config.Name}</Header>
         </div>;
     }
 }
