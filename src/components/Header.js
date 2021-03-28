d8934feaa18a15ebfb3a4d1aff09cc2bc68c7ed9
bkadukun <balaraju_kadukuntla@mentor.com>
2021-03-29 01:54:58 +0530

    Upgraded header to have items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = ({ dark, children, className, to, Link }) => {
+const Header = ({ dark, children, className, pages, to, Link }) => {
     dark = !!dark ? "dark" : "light";
     return (
         <nav
             className={
-                `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
+                `Header navbar navbar-${dark} bg-${dark}` +
+                (className ? " " + className : "") +
+                (pages && pages.length ? " navbar-expand-lg" : "")
             }
         >
             {to ? (
@@ -14,6 +17,21 @@ const Header = ({ dark, children, className, to, Link }) => {
                 </Link>
             ) : (
                 <span className="navbar-brand">{children}</span>
+            )}
+            {pages && pages.length > 0 && (
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        {pages
+                            .filter(a => !a.DontShow)
+                            .map((item, key) => (
+                                <li className="nav-item" key={key}>
+                                    <Link to={item.Path} className="nav-link">
+                                        {item.Name}
+                                    </Link>
+                                </li>
+                            ))}
+                    </ul>
+                </div>
             )}
         </nav>
     );
